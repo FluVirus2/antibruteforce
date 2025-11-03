b19094b1c3069ddcd8cbb7eaadc80c3e0ea0dbaf
Konanau Aliaksandr <pirrolidinil@gmail.com>
2025-11-03 13:00:51 +0300

    feat(client): create mock client backbone

--- mock/FluVirus.AntiBruteForce.Mock.Client/FluVirus.AntiBruteForce.Mock.Client.Cli/Program.cs
+++ mock/FluVirus.AntiBruteForce.Mock.Client/FluVirus.AntiBruteForce.Mock.Client.Cli/Program.cs
@@ -1,9 +1,53 @@
-﻿namespace FluVirus.AntiBruteForce.Mock.Client.Cli;
+﻿using System.Text.Json;
+
+using Microsoft.Extensions.Configuration;
+
+namespace FluVirus.AntiBruteForce.Mock.Client.Cli;
 
 internal class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine($"Hello, World!, {args.Length}");
+        IConfiguration c = new ConfigurationBuilder()
+            .AddEnvironmentVariables(prefix: "DOTNET_")
+            .Build();
+
+        AppConfiguraion configuraion = c.Get<AppConfiguraion>() ?? throw new Exception("cannot bind client configuration");
+
+        using HttpClient client = new()
+        {
+            BaseAddress = new Uri(configuraion.ApiServerUrl)
+        };
+
+        CancellationTokenSource cts = new();
+        Console.CancelKeyPress += (object? sender, ConsoleCancelEventArgs e) => cts.Cancel();
+
+        CancellationToken token = cts.Token;
+        while (!token.IsCancellationRequested)
+        {
+            int randInt = Random.Shared.Next();
+
+            using HttpResponseMessage m = await client.GetAsync($"/echo/{randInt}", cts.Token);
+            if (m.IsSuccessStatusCode)
+            {
+                Stream s = await m.Content.ReadAsStreamAsync(cts.Token);
+                Resource? r = await JsonSerializer.DeserializeAsync<Resource>(s, cancellationToken: cts.Token);
+
+                if (r is not null)
+                {
+                    Console.WriteLine($"By sending {randInt} got {r.Value}");
+                }
+                else
+                {
+                    Console.WriteLine("Got troubles during json deserialization");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Something gone wrong with request, status code - {m.StatusCode}");
+            }
+
+            await Task.Delay(configuraion.Delay ?? 1000);
+        }
     }
 }
---
+++ mock/FluVirus.AntiBruteForce.Mock.Client/FluVirus.AntiBruteForce.Mock.Client.Cli/Resource.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FluVirus.AntiBruteForce.Mock.Client.Cli;
+
+public class Resource
+{
+    [JsonPropertyName("value")]
+    public required int Value { get; init; }
+}
